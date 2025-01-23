17766bfa86fb4b061a298ebef869997f22704ed9
GuiAlvesR <guinegocios20@gmail.com>
2025-01-22 21:37:09 -0300

    Update responsive in section strategy in files css and js

--- js/strategy.js
+++ js/strategy.js
@@ -1,33 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const stepsContainer = document.querySelector(".steps-container");
-  const leftColumn = document.querySelector(".left-column");
-  const rightColumn = document.querySelector(".right-column");
-  const centerColumn = document.querySelector(".center-column");
+  const strategySection = document.querySelector(".strategy");
+  const originalHTML = strategySection.innerHTML;
 
   const adjustLayout = () => {
     if (window.innerWidth <= 768) {
-      let columnsGroup = stepsContainer.querySelector(".columns-group");
-
-      if (!columnsGroup) {
-        columnsGroup = document.createElement("div");
-        columnsGroup.classList.add("columns-group");
-        columnsGroup.appendChild(leftColumn);
-        columnsGroup.appendChild(rightColumn);
-        stepsContainer.appendChild(columnsGroup);
-      }
-
-      if (!stepsContainer.contains(centerColumn)) {
-        stepsContainer.appendChild(centerColumn);
-      }
+      strategySection.innerHTML = `
+       <div class="title">
+          Metodologia
+          <p>
+            Utilizamos uma abordagem estratégica e personalizada para alcançar
+            resultados mensuráveis.
+          </p>
+        </div>
+        
+        <div class="steps-container">
+          <div class="step">
+            <div class="step-number">1</div>
+            <div class="step-content">
+              <span class="subtitle">Análise</span>
+              <p>Bate papo para entendermos qual a situação e o tipo de relacionamento.</p>
+            </div>
+          </div>
+          
+          <div class="step">
+            <div class="step-number">2</div>
+            <div class="step-content">
+              <span class="subtitle">Planejamento</span>
+              <p>Com a aprovação da cliente, é criada uma estratégia para modificar o comportamento, baseada na necessidade do caso, de uma forma que possa gerar gatilhos emocionais no ex/atual.</p>
+            </div>
+          </div>
+          
+          <div class="step">
+            <div class="step-number">3</div>
+            <div class="step-content">
+              <span class="subtitle">Implementação</span>
+              <p>Conforme o avanço do caso, mudaremos as estratégias, caso haja necessidade, respeitando os limites imposto pela cliente.</p>
+            </div>
+          </div>
+          
+          <div class="step">
+            <div class="step-number">4</div>
+            <div class="step-content">
+              <span class="subtitle">Monitoramento e Otimização</span>
+              <p>Acompanhamento diário do desempenho e ajuste das estratégias para otimizar os resultados.</p>
+            </div>
+          </div>
+        </div>
+      `;
     } else {
-      let columnsGroup = stepsContainer.querySelector(".columns-group");
-      if (columnsGroup) {
-        columnsGroup.remove();
-      }
-
-      stepsContainer.appendChild(leftColumn);
-      stepsContainer.appendChild(centerColumn);
-      stepsContainer.appendChild(rightColumn);
+      strategySection.innerHTML = originalHTML;
     }
   };
 
